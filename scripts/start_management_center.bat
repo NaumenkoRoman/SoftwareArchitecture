8803059d454b30e4e10d0a0bbf907421c403f890
NaumenkoRoman <roman.naumenko@ucu.edu.ua>
2024-03-21 17:39:31 +0200

    Project completed.

--- scripts/start_management_center.bat
+++ scripts/start_management_center.bat
@@ -1,1 +1,1 @@
-docker run --network hazelcast-network -p 8080:8080 hazelcast/management-center:latest-snapshot
+docker run --rm --network hazelcast-network -p 8080:8080 hazelcast/management-center:latest-snapshot
--- scripts/start_member.bat
+++ scripts/start_member.bat
@@ -2,4 +2,4 @@
 set HOST=host.docker.internal
 set PORT=%1
 set NAME=%2
-docker run --name %NAME% --network hazelcast-network --rm -e HZ_NETWORK_PUBLICADDRESS=%HOST%:%PORT% -e HZ_CLUSTERNAME=hello-world -p %PORT%:5701 hazelcast/hazelcast:5.3.6
+docker run --name %NAME% --network hazelcast-network --rm -e HZ_QUEUE_QUEUE_MAXSIZE=10 -e HZ_NETWORK_PUBLICADDRESS=%HOST%:%PORT% -e HZ_CLUSTERNAME=hello-world -p %PORT%:5701 hazelcast/hazelcast:5.3.6
